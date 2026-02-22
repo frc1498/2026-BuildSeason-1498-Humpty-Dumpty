40b2ae823c3fdcd0d321e2560f65ebe53e961515
Trevor-G <Trevor.Goolsby@gmail.com>
2026-02-22 11:27:29 -0500

    Commented out the CAN IDs for both rotate motors.  They are now unused. Added a configuration for a zeroing mode for the climber motor.  Includes a supply current of 5 A, a stator current of 50 A, and a maximum voltage output of 3 V.

--- src/main/java/frc/robot/config/ClimberConfig.java
+++ src/main/java/frc/robot/config/ClimberConfig.java
@@ -10,13 +10,14 @@ import com.ctre.phoenix6.signals.SensorDirectionValue;
 public class ClimberConfig {
     //Constants go here
     public static final int kLiftClimbMotorCANID = 17;
-    public static final int kRotateClimb1MotorCANID = 18;
-    public static final int kRotateClimb2MotorCANID = 19;
+    // public static final int kRotateClimb1MotorCANID = 18;
+    // public static final int kRotateClimb2MotorCANID = 19;
     //public static final int kLiftEncoderCANID=0;
     //public static final int kHookRotateEncoderCANID=0;
 
     //Variables
     public TalonFXConfiguration liftClimbMotorConfig;
+    public TalonFXConfiguration liftClimbMotorZeroConfig;
     public TalonFXConfiguration rotateClimb1MotorConfig;
     public TalonFXConfiguration rotateClimb2MotorConfig;
     public CANcoderConfiguration hookRotateCANcoderConfig;
@@ -26,7 +27,10 @@ public class ClimberConfig {
     //Constructor
     public ClimberConfig(){
         liftClimbMotorConfig = new TalonFXConfiguration();  //Instantiate - make a framework
-        this.configureLiftClimber(liftClimbMotorConfig);  //Fill in framework 
+        this.configureLiftClimber(liftClimbMotorConfig);  //Fill in framework
+
+        liftClimbMotorZeroConfig = new TalonFXConfiguration();
+        this.configureLiftClimberZero(liftClimbMotorZeroConfig);
 
         rotateClimb1MotorConfig = new TalonFXConfiguration();  //Instantiate - make a framework
         this.configureRotateClimber1(rotateClimb1MotorConfig);  //Fill in framework 
@@ -79,6 +83,36 @@ public class ClimberConfig {
 
         liftClimber.Voltage.PeakForwardVoltage = 11;
         liftClimber.Voltage.PeakReverseVoltage = -11;
+
+        liftClimber.Audio.AllowMusicDurDisable = true;
+    }
+
+    public void configureLiftClimberZero(TalonFXConfiguration liftClimber){
+        //configure motor
+        liftClimber.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        liftClimber.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        liftClimber.MotorOutput.PeakForwardDutyCycle = 0.25;    // A Maximum output of 3 V would be a 25% duty cycle, assuming a 12 V input.
+        liftClimber.MotorOutput.PeakReverseDutyCycle = -0.25;
+
+        liftClimber.CurrentLimits.StatorCurrentLimit = 50.0;
+        liftClimber.CurrentLimits.StatorCurrentLimitEnable = true;
+        liftClimber.CurrentLimits.SupplyCurrentLimit = 5.0;
+        liftClimber.CurrentLimits.SupplyCurrentLimitEnable = true;
+        liftClimber.CurrentLimits.SupplyCurrentLowerLimit = 5.0;
+        liftClimber.CurrentLimits.SupplyCurrentLowerTime = 1.0;
+
+        //Slot 0 Configs
+        liftClimber.Slot0.kP = 30;  // An error of 1 rotation per second results in 2V output
+        liftClimber.Slot0.kI = 0;  // An error of 1 rotation per second increases output by 0.5V every second
+        liftClimber.Slot0.kD = 0.9;  // A change of 1 rotation per second squared results in 0.01 volts output
+        liftClimber.Slot0.kS = 0;
+        liftClimber.Slot0.kV = 0;  // KV for a Kraken X60 is 490 rpm/V. 490/60 is 8.1667 rps/V.  The inverse is 0.122449 V/rps.
+        liftClimber.Slot0.kA = 0;
+        liftClimber.Slot0.kG = 0;
+        liftClimber.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+
+        liftClimber.Voltage.PeakForwardVoltage = 3;
+        liftClimber.Voltage.PeakReverseVoltage = -3;
 
         liftClimber.Audio.AllowMusicDurDisable = true;
     }
