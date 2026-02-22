b3cc3673de4a5c7fcb2401750730c0e4646713d1
Trevor-G <Trevor.Goolsby@gmail.com>
2026-02-22 11:39:44 -0500

    Lowered the hopper extend setpoint to 14.8, from 15.0.

--- src/main/java/frc/robot/constants/HopperConstants.java
+++ src/main/java/frc/robot/constants/HopperConstants.java
@@ -15,7 +15,7 @@ package frc.robot.constants;
 public class HopperConstants {
 
   //=====================Positions=====================
-  public static final int kHopperExtend = 15;
+  public static final double kHopperExtend = 14.8;
   public static final int kHopperRetract = 0;
   public static final double kDeadband = 0.5;
 
