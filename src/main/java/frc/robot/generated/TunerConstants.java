239728bfbcf8d761bdf8db66e605256fb9529e55
Trevor-G <Trevor.Goolsby@gmail.com>
2026-02-28 14:10:46 -0500

    kD down to 0 from 0.5.

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,7 +24,7 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(25).withKI(0).withKD(0.5)
+        .withKP(25).withKI(0).withKD(0)
         .withKS(0.1).withKV(1.16).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
