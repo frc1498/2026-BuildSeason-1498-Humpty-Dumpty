34139497b326d27ea2f7e9d82af4e74be12c76c2
Tom-1498 <41551603+Tom-1498@users.noreply.github.com>
2026-03-31 14:15:56 -0400

    Update FrontKickupConstants.java

--- src/main/java/frc/robot/constants/FrontKickupConstants.java
+++ src/main/java/frc/robot/constants/FrontKickupConstants.java
@@ -4,12 +4,6 @@
 
 package frc.robot.constants;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
