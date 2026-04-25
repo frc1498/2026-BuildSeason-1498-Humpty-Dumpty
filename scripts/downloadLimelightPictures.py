805cf915cbcbdc769119485935a4c928fc8c6c08
Trevor-G <Trevor.Goolsby@gmail.com>
2026-04-25 15:57:40 -0400

    Silly script to download pictures from the limelight.  Low Priority.

---
+++ scripts/downloadLimelightPictures.py
@@ -0,0 +1,6 @@
+# Navigate to directory
+# Create a limelightLog folder in the directory above this folder
+# Get JSON of limelight photos from limelight
+# For each JSON entry, download the photo from the web address
+# After, delete all pictures
+# Do same for Rewind
