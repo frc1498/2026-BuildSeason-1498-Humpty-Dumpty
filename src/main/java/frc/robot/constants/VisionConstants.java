73e8cd22f8eb1031c27f2d3ed4d20d36a2bc05e7
Tom-1498 <>
2026-04-30 18:02:12 -0400

    Re-set the ambiguity threshold to 0.15

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -75,7 +75,7 @@ public class VisionConstants {
         public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(0.5, 0.5, 9999999);  // Setting these to the same as the Limelight until they can be experimentally found.
         public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 9999999);
 
-        public static final double kAmbiguityThreshold = 1.0;
+        public static final double kAmbiguityThreshold = 0.15;
         public static final int kCompPipelineIndex = 0;         // Treat index '0' as the competition index.
         public static final int kPracticePipelineIndex = 1;     // Treat index '1' as the pipeline index.
 
